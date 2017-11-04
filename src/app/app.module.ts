68d35ea307bcfeb1e91974c9acf6ca48b25413f1
varesc <varesc@gmail.com>
2017-11-04 15:51:52 +0700

    Add class Movie and routing for Movies page and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
