81c6faf283ae4ebc520c12d7a8d96ff71344edf6
varesc <varesc@gmail.com>
2017-11-05 10:41:32 +0700

    create movie service

---
+++ src/app/movie/movie.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Movie} from './movie';
+
+@Injectable()
+
+
+export class MovieService {
+
+    movie: Movie = {
+        id: 1,
+        name: 'Thor'
+    }
+    getMovie(): Promise<Movie> {
+
+        return Promise.resolve(this.movie);
+    }
+}
